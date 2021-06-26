7d8a1d910526bf6789c6c5526dec1f0a2891c695
Maurits <maurits.arissen@student.hu.nl>
2021-06-27 01:27:53 +0200

    Update url.ts

--- src/api/routes/url.ts
+++ src/api/routes/url.ts
@@ -30,7 +30,7 @@ export default (app: Router) => {
       const query: Partial<IUrlInputDTO> = { urlCode: req.params.code };
       const urlServiceInstance = Container.get(UrlService);
       const { url } = await urlServiceInstance.GetUrlByUrlCode(query);
-      return res.status(200).json({ url });
+      return res.redirect(url.longUrl);
     } catch (e) {
       return util.handleCustomError(e, res);
     }
